bde2629e5832e10ff3bcb70d5526601256b153c9
Giwan Yoon <davidmario519@gmail.com>
2024-08-25 19:52:14 +0900

    Design: components 폴더 안에 Description.js 및 css 파일을 생성하고 자기소개 부분 component화 #5

--- src/app/giwan/page.js
+++ src/app/giwan/page.js
@@ -1,17 +1,11 @@
 import Image from 'next/image'
-import myImage from '../../../public/Moodswings in This Order.jpg';
+import myImage from '/public/Moodswings in This Order.jpg';
 import Description from "@/components/giwan/Description";
 
 export default function GiwanPage() {
   return(
     <main>
       <Description/>
-      <Image 
-        src={myImage}
-        alt="image"
-        width={500}
-        height={500}
-        />
     </main>
     
   )
--- src/components/giwan/Description.js
+++ src/components/giwan/Description.js
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./Description.module.css";
+import Image from "next/image";
+import myImage from "/public/Moodswings in This Order.jpg";
 
 export default function Description() {
     return (
         <main className={styles.main}>
-            <h1>안녕하세요. 저는 윤기완입니다!</h1>
-            <p>제 MBTI는 INFJ입니다.</p>
-            <p>이번 ATC 웹개발팀 팀원분들과 친해지고 싶습니다..!</p>
-            <p>잘 부탁드립니다 </p>
+            <h1 className={styles.h1}>안녕하세요. 저는 윤기완입니다!</h1>
+            <div className={styles.p}>
+                <p>제 MBTI는 INFJ입니다.</p>
+                <p>이번 ATC 웹개발팀 팀원분들과 친해지고 싶습니다..!</p>
+                <p>잘 부탁드립니다 :)</p>
+            </div>
+            <Image src={myImage} alt="image" width={500} height={500} />
         </main>
     );
 }
