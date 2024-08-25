da28fa0c307d1b4bd0198895b71a137590c4bb16
Giwan Yoon <davidmario519@gmail.com>
2024-08-25 19:41:40 +0900

    Design: className으로 class 연결 #5

--- src/app/giwan/page.js
+++ src/app/giwan/page.js
@@ -1,14 +1,11 @@
-import styles from "./module.css";
 import Image from 'next/image'
 import myImage from '../../../public/Moodswings in This Order.jpg';
+import Description from "@/components/giwan/Description";
 
 export default function GiwanPage() {
   return(
     <main>
-      <h1>안녕하세요. 저는 윤기완입니다!</h1>
-      <p>제 MBTI는 INFJ입니다.</p>
-      <p>이번 ATC 웹개발팀 팀원분들과 친해지고 싶습니다..!</p>
-      <p>잘 부탁드립니다 :)</p>
+      <Description/>
       <Image 
         src={myImage}
         alt="image"
---
+++ src/components/giwan/Description.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Description.module.css";
+
+export default function Description() {
+    return (
+        <main className={styles.main}>
+            <h1>안녕하세요. 저는 윤기완입니다!</h1>
+            <p>제 MBTI는 INFJ입니다.</p>
+            <p>이번 ATC 웹개발팀 팀원분들과 친해지고 싶습니다..!</p>
+            <p>잘 부탁드립니다 </p>
+        </main>
+    );
+}
